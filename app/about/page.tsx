eefa234507baaddbb032ec94279642b189a15322
deepakdnayak <deepakdnayak2004@gmail.com>
2025-01-27 10:02:46 +0530

    Added Sample API

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
 
 const about = () => {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const response = await fetch("/api/hello");
+      const data = await response.json();
+      setMessage(data.message);
+    };
+
+    fetchMessage();
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-green-600">This is the About Page!</h1>
+      <h1 className="text-4xl font-bold text-green-600">{message || "Loading..."}</h1>
     </div>
   )
 }
